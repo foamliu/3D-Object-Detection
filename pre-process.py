fc6a082b08bd6d21ddf6817a9dbf8d87aef24524
Yang Liu <foamliu@yeah.net>
2018-05-19 23:04:24 +0800

    avoid occupy too much spaces

--- pre-process.py
+++ pre-process.py
@@ -5,76 +5,78 @@ import shutil
 import tarfile
 
 if __name__ == '__main__':
-    area_nos = [1, 2, 3, 4, 6]
 
-    for area_no in area_nos:
-        tar_file = 'area_{}_no_xyz.tar'.format(area_no)
-        filename = os.path.join('data', tar_file)
-        print('Extracting {}...'.format(filename))
+    if not os.path.exists('data/rgb'):
+        area_nos = [1, 2, 3, 4, 6]
+        for area_no in area_nos:
+            tar_file = 'area_{}_no_xyz.tar'.format(area_no)
+            filename = os.path.join('data', tar_file)
+            print('Extracting {}...'.format(filename))
 
-        with tarfile.open(filename) as tar:
-            tar.extractall()
+            with tarfile.open(filename) as tar:
+                tar.extractall()
 
-        folder = 'area_{}'.format(area_no)
-        if not os.path.exists('data/rgb'):
-            os.makedirs('data/rgb')
-            for f in [f for f in os.listdir(os.path.join(folder, 'data/rgb')) if f.endswith('.png')]:
-                src_path = os.path.join(folder, 'data/rgb')
-                src_path = os.path.join(src_path, f)
-                dst_path = 'data/rgb/'
-                shutil.move(src_path, dst_path)
+            folder = 'area_{}'.format(area_no)
+            if not os.path.exists('data/rgb'):
+                os.makedirs('data/rgb')
+                for f in [f for f in os.listdir(os.path.join(folder, 'data/rgb')) if f.endswith('.png')]:
+                    src_path = os.path.join(folder, 'data/rgb')
+                    src_path = os.path.join(src_path, f)
+                    dst_path = 'data/rgb/'
+                    shutil.move(src_path, dst_path)
 
-        if not os.path.exists('data/depth'):
-            os.makedirs('data/depth')
-            for f in [f for f in os.listdir(os.path.join(folder, 'data/depth')) if f.endswith('.png')]:
-                src_path = os.path.join(folder, 'data/depth')
-                src_path = os.path.join(src_path, f)
-                dst_path = 'data/depth/'
-                shutil.move(src_path, dst_path)
+            if not os.path.exists('data/depth'):
+                os.makedirs('data/depth')
+                for f in [f for f in os.listdir(os.path.join(folder, 'data/depth')) if f.endswith('.png')]:
+                    src_path = os.path.join(folder, 'data/depth')
+                    src_path = os.path.join(src_path, f)
+                    dst_path = 'data/depth/'
+                    shutil.move(src_path, dst_path)
 
-        if not os.path.exists('data/semantic'):
-            os.makedirs('data/semantic')
-            for f in [f for f in os.listdir(os.path.join(folder, 'data/semantic_pretty')) if f.endswith('.png')]:
-                src_path = os.path.join(folder, 'data/semantic_pretty')
-                src_path = os.path.join(src_path, f)
-                dst_path = 'data/semantic/'
-                shutil.move(src_path, dst_path)
+            if not os.path.exists('data/semantic'):
+                os.makedirs('data/semantic')
+                for f in [f for f in os.listdir(os.path.join(folder, 'data/semantic_pretty')) if f.endswith('.png')]:
+                    src_path = os.path.join(folder, 'data/semantic_pretty')
+                    src_path = os.path.join(src_path, f)
+                    dst_path = 'data/semantic/'
+                    shutil.move(src_path, dst_path)
 
         shutil.rmtree(folder)
 
-    area_nos = ['5a', '5b']
-    for area_no in area_nos:
-        tar_file = 'area_{}_no_xyz.tar'.format(area_no)
-        filename = os.path.join('data', tar_file)
-        print('Extracting {}...'.format(filename))
+    if not os.path.exists('data/rgb_test'):
+        area_nos = ['5a', '5b']
+        for area_no in area_nos:
+            tar_file = 'area_{}_no_xyz.tar'.format(area_no)
+            filename = os.path.join('data', tar_file)
+            print('Extracting {}...'.format(filename))
 
-        with tarfile.open(filename) as tar:
-            tar.extractall()
+            with tarfile.open(filename) as tar:
+                tar.extractall()
 
-        folder = 'area_{}'.format(area_no)
-        if not os.path.exists('data/rgb_test'):
-            os.makedirs('data/rgb_test')
-            for f in [f for f in os.listdir(os.path.join(folder, 'data/rgb')) if f.endswith('.png')]:
-                src_path = os.path.join(folder, 'data/rgb')
-                src_path = os.path.join(src_path, f)
-                dst_path = 'data/rgb_test/'
-                shutil.move(src_path, dst_path)
+            folder = 'area_{}'.format(area_no)
+            if not os.path.exists('data/rgb_test'):
+                os.makedirs('data/rgb_test')
+                for f in [f for f in os.listdir(os.path.join(folder, 'data/rgb')) if f.endswith('.png')]:
+                    src_path = os.path.join(folder, 'data/rgb')
+                    src_path = os.path.join(src_path, f)
+                    dst_path = 'data/rgb_test/'
+                    shutil.move(src_path, dst_path)
 
-        if not os.path.exists('data/depth_test'):
-            os.makedirs('data/depth_test')
-            for f in [f for f in os.listdir(os.path.join(folder, 'data/depth')) if f.endswith('.png')]:
-                src_path = os.path.join(folder, 'data/depth')
-                src_path = os.path.join(src_path, f)
-                dst_path = 'data/depth_test/'
-                shutil.move(src_path, dst_path)
+            if not os.path.exists('data/depth_test'):
+                os.makedirs('data/depth_test')
+                for f in [f for f in os.listdir(os.path.join(folder, 'data/depth')) if f.endswith('.png')]:
+                    src_path = os.path.join(folder, 'data/depth')
+                    src_path = os.path.join(src_path, f)
+                    dst_path = 'data/depth_test/'
+                    shutil.move(src_path, dst_path)
 
-        if not os.path.exists('data/semantic_test'):
-            os.makedirs('data/semantic_test')
-            for f in [f for f in os.listdir(os.path.join(folder, 'data/semantic_pretty')) if f.endswith('.png')]:
-                src_path = os.path.join(folder, 'data/semantic_pretty')
-                src_path = os.path.join(src_path, f)
-                dst_path = 'data/semantic_test/'
-                shutil.move(src_path, dst_path)
+            if not os.path.exists('data/semantic_test'):
+                os.makedirs('data/semantic_test')
+                for f in [f for f in os.listdir(os.path.join(folder, 'data/semantic_pretty')) if f.endswith('.png')]:
+                    src_path = os.path.join(folder, 'data/semantic_pretty')
+                    src_path = os.path.join(src_path, f)
+                    dst_path = 'data/semantic_test/'
+                    shutil.move(src_path, dst_path)
 
     image_names = [f for f in os.listdir('data/rgb') if f.endswith('.png')]
     print('{} images'.format(len(image_names)))
